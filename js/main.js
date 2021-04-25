34294351528696d858c2b36a1b5ef947d28afcee
Sergey <zyakin33@yandex.ru>
2021-04-26 00:57:51 +0300

    add menu burger

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper1 = document.querySelector('.swiper-container'),
+      burger = document.querySelector('.burger'),
+      menuClose = document.querySelector('.menu__close'),
+      menu = document.querySelector('.menu');
+
+const swiper = new Swiper(swiper1, {
   centeredSlides: true,
   slidesPerView: 'auto',
   loop: true,
   // speed: 400,
   spaceBetween: 105,
+});
+
+burger.addEventListener('click', () => {
+  menu.classList.add('menu--visible');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--visible');
 });
